209a773b569d36d0c10f561d520a5bc011f5fc9c
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-04-25 21:23:43 -0400

    feat: set up tailwind

---
+++ src/components/base/Button.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  label: String,
+  onClick: Function,
+  disabled: Boolean,
+  variant: 'primary' | 'secondary',
+})
+</script>
+
+<template>
+  <button type="button" @click="onClick">{{ label }}</button>
+</template>
+
+<style scoped>
+
+</style>
