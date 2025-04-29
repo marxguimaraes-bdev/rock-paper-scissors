793f22ea1b6d16b9fc558107b660da17a8c24942
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-04-28 20:16:42 -0400

    fix: destructure props

--- src/components/base/Button/Button.vue
+++ src/components/base/Button/Button.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const props = defineProps<{
+const { label, onClick, disabled, variant, size } = defineProps<{
   label: string;
   onClick: () => void;
   disabled: boolean;
@@ -15,11 +15,11 @@ const props = defineProps<{
     :class="{
       'linear-ease cursor-pointer rounded-[0.5rem] border font-barlow text-[1rem] leading-[1rem] font-semibold tracking-[2.5px] uppercase transition-colors duration-300': true,
       'border-blue bg-white text-blue hover:border-red hover:text-red':
-        props.variant === 'primary',
+        variant === 'primary',
       'border-white bg-blue text-white hover:border-blue hover:bg-white hover:text-blue':
-        props.variant === 'secondary',
-      'px-[3.75rem] pt-[0.9375rem] pb-[0.875rem]': props.size === 'large',
-      'px-[2.25rem] pt-[0.6875rem] pb-[0.625rem]': props.size === 'small',
+        variant === 'secondary',
+      'px-[3.75rem] pt-[0.9375rem] pb-[0.875rem]': size === 'large',
+      'px-[2.25rem] pt-[0.6875rem] pb-[0.625rem]': size === 'small',
     }"
     :disabled="disabled"
   >
