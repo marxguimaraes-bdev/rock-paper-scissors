9b4d99c39591a8b190a0fba2e3c0fb711e587ee3
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-04-29 20:02:25 -0400

    fix: rename colors in css file

--- src/components/base/Button/Button.vue
+++ src/components/base/Button/Button.vue
@@ -14,9 +14,9 @@ const { label, onClick, disabled, variant, size } = defineProps<{
     @click="onClick"
     :class="{
       'linear-ease cursor-pointer rounded-[0.5rem] border font-barlow text-[1rem] leading-[1rem] font-semibold tracking-[2.5px] uppercase transition-colors duration-300': true,
-      'border-blue bg-white text-blue hover:border-red hover:text-red':
+      'border-blue-100 bg-white text-blue-100 hover:border-red-100 hover:text-red-100':
         variant === 'primary',
-      'border-white bg-blue text-white hover:border-blue hover:bg-white hover:text-blue':
+      'hover:border-blue border-white bg-blue-100 text-white hover:bg-white hover:text-blue-100':
         variant === 'secondary',
       'px-[3.75rem] pt-[0.9375rem] pb-[0.875rem]': size === 'large',
       'px-[2.25rem] pt-[0.6875rem] pb-[0.625rem]': size === 'small',
