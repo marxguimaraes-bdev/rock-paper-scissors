e8a7ce44d5124d33930a9f152defb7ce0aa444ed
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-05-07 18:07:53 -0400

    feat: add winner and play again button to show result

--- src/components/Compound/ShowResult/ShowResult.vue
+++ src/components/Compound/ShowResult/ShowResult.vue
@@ -18,11 +18,18 @@ const [move1, move2] = moves;
 
 <template>
   <section
-    class="grid grid-cols-2 items-center justify-center gap-x-[50px] gap-y-[17px] text-center font-barlow text-[15px] leading-[32px] font-bold tracking-[1.88px] text-white uppercase sm:gap-x-[72.39px] sm:gap-y-[63px] sm:text-[24px] sm:tracking-[3px]"
+    class="grid grid-cols-2 items-center justify-center gap-x-[50px] gap-y-[17px] text-center font-barlow text-[15px] leading-[32px] font-bold tracking-[1.88px] text-white uppercase sm:text-[24px] sm:tracking-[3px] xl:grid-cols-3 xl:gap-x-[72.39px] xl:gap-y-[63px]"
   >
-    <div class="row-start-2 sm:row-start-1">You picked</div>
-    <div class="row-start-2 sm:row-start-1">The house picked</div>
-    <ButtonRound :variant="move1.type" size="large" @click="() => {}">
+    <div class="row-start-2 xl:col-start-1 xl:row-start-1">You picked</div>
+    <div class="row-start-2 xl:col-start-3 xl:row-start-1">
+      The house picked
+    </div>
+    <ButtonRound
+      class="justify-self-center xl:col-start-1"
+      :variant="move1.type"
+      size="large"
+      @click="() => {}"
+    >
       <RockSvg
         v-if="move1.type === 'rock'"
         class="h-[3.125rem] w-[3.125rem] place-self-center sm:h-[110px] sm:w-[110px]"
@@ -36,7 +43,12 @@ const [move1, move2] = moves;
         class="h-[3.125rem] w-[3.125rem] place-self-center sm:h-[141.87px] sm:w-[141.87px]"
       />
     </ButtonRound>
-    <ButtonRound :variant="move2.type" size="large" @click="() => {}">
+    <ButtonRound
+      class="justify-self-center xl:col-start-3"
+      :variant="move2.type"
+      size="large"
+      @click="() => {}"
+    >
       <RockSvg
         v-if="move2.type === 'rock'"
         class="h-[3.125rem] w-[3.125rem] place-self-center sm:h-[110px] sm:w-[110px]"
@@ -50,5 +62,17 @@ const [move1, move2] = moves;
         class="h-[3.125rem] w-[3.125rem] place-self-center sm:h-[141.87px] sm:w-[141.87px]"
       />
     </ButtonRound>
+    <div
+      class="col-span-2 self-center text-[56px] leading-[67px] tracking-normal xl:absolute xl:col-span-1 xl:col-start-2 xl:row-start-1 xl:mt-[160px] xl:self-start xl:justify-self-center"
+    >
+      {{ winnerLabel }}
+    </div>
+    <Button
+      class="col-span-2 mr-[78px] ml-[77px] xl:col-span-1 xl:col-start-2 xl:row-start-2 xl:mb-[104px] xl:self-end"
+      label="Play again"
+      variant="primary"
+      size="large"
+      @click="() => {}"
+    />
   </section>
 </template>
