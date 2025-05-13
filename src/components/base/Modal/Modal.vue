e776553fd5c1f311c04e65ea89bbe2d84b07f160
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-05-12 21:21:17 -0400

    feat: add transition to modal component

--- src/components/base/Modal/Modal.vue
+++ src/components/base/Modal/Modal.vue
@@ -9,20 +9,34 @@ const emit = defineEmits(['toggle']);
 </script>
 
 <template>
-  <div
-    v-if="visible"
-    class="fixed top-0 left-0 z-30 flex h-screen w-screen items-center justify-center bg-transparent-black"
-  >
+  <Transition name="fade">
     <div
-      class="z-40 mx-auto grid h-full w-full bg-white sm:h-[415px] sm:w-[400px] sm:rounded-[8px]"
+      v-if="visible"
+      class="fixed top-0 left-0 z-30 flex h-screen w-screen items-center justify-center bg-transparent-black"
     >
-      <slot />
-      <button
-        class="mb-[66px] self-end justify-self-center hover:cursor-pointer sm:absolute sm:mt-[38px] sm:mr-[32px] sm:self-start sm:justify-self-end"
-        @click="emit('toggle')"
+      <div
+        class="z-40 mx-auto grid h-full w-full bg-white sm:h-[415px] sm:w-[400px] sm:rounded-[8px]"
       >
-        <CloseIcon />
-      </button>
+        <slot />
+        <button
+          class="mb-[66px] self-end justify-self-center hover:cursor-pointer sm:absolute sm:mt-[38px] sm:mr-[32px] sm:self-start sm:justify-self-end"
+          @click="() => emit('toggle')"
+        >
+          <CloseIcon />
+        </button>
+      </div>
     </div>
-  </div>
+  </Transition>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 150ms ease-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
